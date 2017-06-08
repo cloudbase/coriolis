2e532f2c3623b8280a2925a5326286a303e93477
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2017-06-08 10:59:00 +0000

    fix ID for rhel

--- coriolis/osmorphing/osmount/redhat.py
+++ coriolis/osmorphing/osmount/redhat.py
@@ -15,7 +15,7 @@ class RedHatOSMountTools(base.BaseLinuxOSMountTools):
         os_info = utils.get_linux_os_info(self._ssh)
         if os_info and os_info[0] in [
                 'RedHatEnterpriseServer', 'CentOS', 'OracleServer',
-                'redhat', 'centos', 'ol']:
+                'rhel', 'centos', 'ol']:
             return True
 
     def _pre_mount_os(self):
