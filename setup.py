77c44071eda2e5a4b8e10289a00752486d4a7936
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2017-07-04 20:36:07 +0300

    add coriolis-dbsync command
    * add __init__.py in cmd folder
    * add coriolis-dbsync command
    * move resources under coriolis folder
    * modify setup.py to build the write_data binary on Linux
    * add MANIFEST.in to include the binary

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 import setuptools
+import subprocess
+import platform
+import os
 
 
+def _compile_and_install():
+    if platform.system() != "Linux":
+        return
+    dirname = os.path.dirname(__file__)
+    resources = os.path.join(dirname, "coriolis", "resources")
+    if os.path.isdir(resources):
+        subprocess.check_call(
+            ["make"], cwd=resources, shell=True)
+
+
+_compile_and_install()
 setuptools.setup(
     setup_requires=['pbr>=1.8'],
     pbr=True)
