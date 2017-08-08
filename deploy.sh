9395f99a5c34f26432ffeedc115f5c9621137b4b
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-08-09 00:56:42 +0300

    Add config.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
-kolla_internal_vip_address=$1
-ansible-playbook deploy.yml -e @/etc/kolla/passwords.yml \
--e "kolla_internal_vip_address=$kolla_internal_vip_address"
+
+basedir=$(dirname "$(readlink -f "$0")")
+
+iface=${1:-ens160}
+
+set_config_value=$basedir/set_config_value.py
+config_file=$basedir/config.yml
+
+VIP=$(/sbin/ip -4 -o addr show dev $iface | awk '{split($4,a,"/");print a[1]}')
+python $set_config_value -c $config_file -n coriolis_host -v $VIP
+
+ansible-playbook $basedir/deploy.yml \
+-e @/etc/kolla/passwords.yml \
+-e @$config_file
