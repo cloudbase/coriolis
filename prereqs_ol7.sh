3e78a3971bc91f7512188f17979398c642df9c32
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-08-11 02:58:30 +0300

    Add HTTP and HTTPS ports to firewall rules

--- prereqs_ol7.sh
+++ prereqs_ol7.sh
@@ -43,3 +43,5 @@ firewall-cmd --permanent --zone=public --add-port=35357/tcp
 firewall-cmd --permanent --zone=public --add-port=5000/tcp
 firewall-cmd --permanent --zone=public --add-port=9311/tcp
 firewall-cmd --permanent --zone=public --add-port=7667/tcp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=443/tcp
