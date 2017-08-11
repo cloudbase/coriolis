263156767412065588ab3a5183f3f5cc3f582c17
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-08-11 04:54:36 +0300

    Additional Kolla cleanup

--- kolla/destroy.sh
+++ kolla/destroy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -e
 
 kolla-ansible destroy --yes-i-really-really-mean-it ./kolla-ansible/ansible/inventory/coriolis
+
+docker volume rm mariadb
+docker volume rm rabbitmq
+docker volume rm kolla_logs
+
+rm -rf /etc/kolla/
