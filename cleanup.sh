7ada7842591fab8877df5a7434e5ba38a591c21d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-08-11 05:32:04 +0300

    Fix cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,16 @@
 docker rm -f coriolis-worker
 docker rm -f coriolis-conductor
 docker rm -f coriolis-api
+docker rm -f coriolis-web
+docker rm -f coriolis-web-proxy
 
-docker rmi coriolis-worker
-docker rmi coriolis-conductor
-docker rmi coriolis-api
-docker rmi coriolis-common
-docker rmi coriolis-base
+TAG=1.0.0
+REPO=registry.cloudbase.it
+
+docker rmi $REPO/coriolis-worker:$TAG
+docker rmi $REPO/coriolis-conductor:$TAG
+docker rmi $REPO/coriolis-api:$TAG
+docker rmi $REPO/coriolis-common:$TAG
+docker rmi $REPO/coriolis-base:$TAG
+docker rmi $REPO/coriolis-web:$TAG
+docker rmi $REPO/coriolis-web-proxy:$TAG
