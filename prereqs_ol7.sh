7b8aee982556c94994b46b760ef79be6d77bb710
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2017-08-11 03:57:33 +0300

    Use HTTPS in coriolis-web-proxy

--- prereqs_ol7.sh
+++ prereqs_ol7.sh
@@ -45,3 +45,4 @@ firewall-cmd --permanent --zone=public --add-port=9311/tcp
 firewall-cmd --permanent --zone=public --add-port=7667/tcp
 firewall-cmd --permanent --zone=public --add-port=80/tcp
 firewall-cmd --permanent --zone=public --add-port=443/tcp
+firewall-cmd --reload
