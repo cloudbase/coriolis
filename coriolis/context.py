ffc54cd351d27c3047cf4402854d3e25829872e6
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-05-16 15:14:09 +0300

    Add user/project domain params to RequestContext.
    Add an individual parameter for both the name and ID of the domain
    for both the user and project when using Keystone v3.

--- coriolis/context.py
+++ coriolis/context.py
@@ -11,16 +11,23 @@ class RequestContext(context.RequestContext):
     def __init__(self, user, tenant, is_admin=None,
                  roles=None, project_name=None, remote_address=None,
                  timestamp=None, request_id=None, auth_token=None,
-                 overwrite=True, domain=None, user_domain=None,
-                 project_domain=None, show_deleted=None, trust_id=None,
+                 overwrite=True, domain_name=None, domain_id=None,
+                 user_domain_name=None, user_domain_id=None,
+                 project_domain_name=None, project_domain_id=None,
+                 show_deleted=None, trust_id=None,
                  delete_trust_id=False, **kwargs):
 
         super(RequestContext, self).__init__(auth_token=auth_token,
                                              user=user,
                                              tenant=tenant,
-                                             domain=domain,
-                                             user_domain=user_domain,
-                                             project_domain=project_domain,
+                                             domain_name=domain_name,
+                                             domain_id=domain_id,
+                                             user_domain_name=user_domain_name,
+                                             user_domain_id=user_domain_id,
+                                             project_domain_name=(
+                                                 project_domain_name),
+                                             project_domain_id=(
+                                                 project_domain_id),
                                              is_admin=is_admin,
                                              show_deleted=show_deleted,
                                              request_id=request_id,
@@ -41,7 +48,12 @@ class RequestContext(context.RequestContext):
         result['user'] = self.user
         result['tenant'] = self.tenant
         result['project_name'] = self.project_name
-        result['domain'] = self.domain
+        result['domain_id'] = self.domain_id
+        result['domain_name'] = self.domain_name
+        result['user_domain_id'] = self.user_domain_id
+        result['user_domain_name'] = self.user_domain_name
+        result['project_domain_id'] = self.project_domain_id
+        result['project_domain_name'] = self.project_domain_name
         result['roles'] = self.roles
         result['remote_address'] = self.remote_address
         result['timestamp'] = self.timestamp.isoformat()
