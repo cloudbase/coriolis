fbc589fe721cea3a23f444283cf8e8c04d773a27
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-07-17 13:45:48 +0300

    Add IPv4 forwarding config.

--- prereqs_ol7.sh
+++ prereqs_ol7.sh
@@ -52,6 +52,10 @@ yum install docker-engine --enablerepo=ol7_addons -y
 systemctl enable docker
 systemctl start docker
 
+# NOTE: IPv4 forwarding is required for rebuilding container images
+# and resolving DNS inside the running containers:
+echo "net.ipv4.ip_forward=1" > 99-docker-ipv4-forwarding.conf
+
 yum install git -y
 yum groupinstall development tools -y
 
