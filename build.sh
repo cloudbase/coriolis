44f9f0f2547781b3a10a677b3cfc949894b7a152
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-09-20 18:02:37 +0300

    Uninstall `docker` and install `docker-py` in build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ basedir=$(dirname "$(readlink -f "$0")")
 
 config_file=$basedir/config-build.yml
 
+# NOTE: Ansible requires that `docker-py` be installed
+# instead of the newer `docker` when building images
+if [ "$(pip freeze | grep docker==)" ]; then
+    pip uninstall -y docker
+fi
+pip install --upgrade --force-reinstall docker-py
+
 if [ ! -f $config_file ]; then
     cp $config_file.sample $config_file
 fi
