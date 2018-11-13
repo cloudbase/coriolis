cb200c616eb3734321874b5fe6da00c4aa55b4f6
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-11-13 13:37:56 +0200

    Add 'storage_mappings' field to Migration/Replica API.
    The 'storage_mappings' field will be passed to the provider plugins in
    order to determine what storage backends to use for each of the
    migrated/replicated VM disks.
    
    The current implementation simply appends the 'storage_mappings' to the
    'destination_environment' options, though an overhaul of the provider
    interfaces to support (and allow for ease of addition of new options)
    should be performed in the long run.

---
+++ coriolis/api/v1/utils.py
@@ -0,0 +1,31 @@
+# Copyright 2018 Cloudbase Solutions Srl
+# All Rights Reserved.
+
+from oslo_log import log as logging
+from webob import exc
+
+from coriolis import exception
+from coriolis import schemas
+
+
+LOG = logging.getLogger(__name__)
+
+
+def validate_network_map(network_map):
+    """ Validates the JSON schema for the network_map. """
+    try:
+        schemas.validate_value(
+            network_map, schemas.CORIOLIS_NETWORK_MAP_SCHEMA)
+    except exception.SchemaValidationException as ex:
+        raise exc.HTTPBadRequest(
+            explanation="Invalid network_map: %s" % str(ex))
+
+
+def validate_storage_mappings(storage_mappings):
+    """ Validates the JSON schema for the storage_mappings. """
+    try:
+        schemas.validate_value(
+            storage_mappings, schemas.CORIOLIS_STORAGE_MAPPINGS_SCHEMA)
+    except exception.SchemaValidationException as ex:
+        raise exc.HTTPBadRequest(
+            explanation="Invalid storage_mappings: %s" % str(ex))
