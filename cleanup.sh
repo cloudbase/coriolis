a24ce1041a4122b4f9b7b2be5f6ddc9dfc74848a
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-11-29 17:45:28 +0200

    Add support for building Docker images based on Ubuntu 18.04.

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# USAGE: $0 [IMAGES_DOCKER_TAG [IMAGES_DOCKER_REPO]]
-# EX: $0 1.0.1-dev registry.cloudbase.it
+# USAGE: $0 [IMAGES_DOCKER_TAG [IMAGES_DOCKER_TEMPLATE] [IMAGES_DOCKER_REPO]]
+# EX: $0 1.0.1-dev ol7 registry.cloudbase.it
 
 basedir=$(dirname "$(readlink -f "$0")")
 get_config_value=$basedir/get_config_value.py
@@ -10,7 +10,7 @@ CONF_BUILD_FILE="$basedir/config-build.yml"
 if test ! -e $CONF_BUILD_FILE; then
     if test ! "$1" && test ! "$2"; then
         echo "$CONF_BUILD_FILE not present, please rerun with:"
-        echo "$0 <Docker tag> <Docker registry URL>"
+        echo "$0 <Docker tag> <Docker template (ol7/ubuntu18.04)> <Docker registry URL>"
         exit 1
     fi
 fi
@@ -26,7 +26,11 @@ TAG="$1"
 if test ! "$TAG"; then
     TAG=`python $get_config_value -c $CONF_BUILD_FILE -n docker_images_tag`
 fi
-REPO="$2"
+TEMPLATE="$2"
+if test ! "$TEMPLATE"; then
+    TEMPLATE=`python $get_config_value -c $CONF_BUILD_FILE -n docker_template`
+fi
+REPO="$3"
 if test ! "$REPO"; then
     REPO=`python $get_config_value -c $CONF_BUILD_FILE -n docker_registry`
     NS=`python $get_config_value -c $CONF_BUILD_FILE -n coriolis_containers_namespace`
@@ -35,11 +39,11 @@ if test ! "$REPO"; then
     fi
 fi
 
-docker rmi $REPO/coriolis-worker:$TAG
-docker rmi $REPO/coriolis-conductor:$TAG
-docker rmi $REPO/coriolis-api:$TAG
-docker rmi $REPO/coriolis-common:$TAG
-docker rmi $REPO/coriolis-base:$TAG
-docker rmi $REPO/coriolis-web:$TAG
-docker rmi $REPO/coriolis-web-proxy:$TAG
-docker rmi $REPO/coriolis-replica-cron:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-worker:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-conductor:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-api:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-common:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-base:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-web:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-web-proxy:$TAG
+docker rmi $REPO/$TEMPLATE/coriolis-replica-cron:$TAG
