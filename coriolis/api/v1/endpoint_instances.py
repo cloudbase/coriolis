32213759bc6af4ce38612836ccc0e8ff153ac387
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2019-08-05 15:25:28 +0300

    Add 'env' option during get_instance(s) API calls

--- coriolis/api/v1/endpoint_instances.py
+++ coriolis/api/v1/endpoint_instances.py
@@ -2,7 +2,6 @@
 # All Rights Reserved.
 
 from oslo_log import log as logging
-from webob import exc
 
 from coriolis import utils
 from coriolis.api import common
@@ -26,9 +25,16 @@ class EndpointInstanceController(api_wsgi.Controller):
         marker, limit = common.get_paging_params(req)
         instance_name_pattern = req.GET.get("name")
 
+        env = req.GET.get("env")
+        if env is not None:
+            env = utils.decode_base64_param(env, is_json=True)
+        else:
+            env = {}
+
         return endpoint_instance_view.collection(
             req, self._instance_api.get_endpoint_instances(
-                context, endpoint_id, marker, limit, instance_name_pattern))
+                context, endpoint_id, env, marker, limit,
+                instance_name_pattern))
 
     def show(self, req, endpoint_id, id):
         context = req.environ['coriolis.context']
@@ -39,9 +45,15 @@ class EndpointInstanceController(api_wsgi.Controller):
         # See e.g.: https://github.com/pallets/flask/issues/900
         id = utils.decode_base64_param(id)
 
+        env = req.GET.get("env")
+        if env is not None:
+            env = utils.decode_base64_param(env, is_json=True)
+        else:
+            env = {}
+
         return endpoint_instance_view.single(
             req, self._instance_api.get_endpoint_instance(
-                req.environ['coriolis.context'], endpoint_id, id))
+                req.environ['coriolis.context'], endpoint_id, env, id))
 
 
 def create_resource():
