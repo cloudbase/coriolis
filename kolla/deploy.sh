8322400fb19f1d73fa8034c3e0b1fed0870b830a
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2019-08-22 05:37:37 +0000

    Refactor Kolla build / deploy scripts
    * Use `utils/common.sh` code whenever possible
    * General scripts cleanup
    * Use `docker_images_tag` from the Coriolis `config-build.yml`
      to tag newly built the Kolla images

--- kolla/deploy.sh
+++ kolla/deploy.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -e
 
-basedir=$(dirname "$(readlink -f "$0")")
+BASE_DIR=$(dirname "$(readlink -f "$0")")
+source "$BASE_DIR/../utils/common.sh"
+
 if [ -d /root/kolla-ansible ]; then
-    if [ -d $basedir/kolla-ansible ]; then
-        echo "ERROR: 'kolla-ansible' directory found both in /root and $basedir."
-        echo "Please pick the latest one used and move it in $basedir."
+    if [ -d "$BASE_DIR/kolla-ansible" ]; then
+        echo "ERROR: 'kolla-ansible' directory found both in /root and $BASE_DIR."
+        echo "Please pick the latest one used and move it in $BASE_DIR."
         exit 2
     fi
-    echo "WARN: old kolla-ansible found in /root, moving it to $basedir."
-    mv /root/kolla-ansible $basedir
+    echo "WARN: old kolla-ansible found in /root, moving it to $BASE_DIR."
+    mv /root/kolla-ansible "$BASE_DIR"
 fi
-pushd "$basedir"
 
-distro=${1:-oraclelinux}
-# Defaults to localhost
-iface=${2:-lo}
+pushd "$BASE_DIR"
+
+DISTRO=${1:-oraclelinux}
+IFACE=${2:-lo} # Defaults to localhost
 
 if [ ! -d ./kolla-ansible ]; then
     git clone https://github.com/openstack/kolla-ansible -b stable/ocata
-
     pip install -q ./kolla-ansible
 fi
 
@@ -33,38 +34,33 @@ fi
 
 cp ./coriolis ./kolla-ansible/ansible/inventory/
 
-set_config_value=../set_config_value.py
-
-VIP=$(/sbin/ip -4 -o addr show dev $iface | awk '{split($4,a,"/");print a[1]}')
+VIP=$(/sbin/ip -4 -o addr show dev $IFACE | awk '{split($4,a,"/");print a[1]}')
 if [ ! "$VIP" ]; then
-    echo "Could not find IP for interface $iface"
+    echo "Could not find IP for interface $IFACE"
     exit 1
 fi
-python $set_config_value -c /etc/kolla/globals.yml -n kolla_internal_vip_address -v $VIP
-python $set_config_value -c /etc/kolla/globals.yml -n network_interface -v $iface
-python $set_config_value -c /etc/kolla/globals.yml -n kolla_base_distro -v $distro
+python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n kolla_internal_vip_address -v $VIP
+python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n network_interface -v $IFACE
+python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n kolla_base_distro -v $DISTRO
 
-config_build_file=../config-build.yml
-if [ ! -e "$config_build_file" ]; then
-    cp $config_build_file.sample $config_build_file
-fi
-get_config_value=../get_config_value.py
-kolla_containers_namespace=`python $get_config_value -c $config_build_file -n kolla_containers_namespace`
+new_config_file "$CONFIG_BUILD_FILE"
+
+kolla_containers_namespace=$(python "$GET_CONFIG_VALUE_SCRIPT" -c "$CONFIG_BUILD_FILE" -n kolla_containers_namespace)
 if [ "$kolla_containers_namespace" ]; then
-    python $set_config_value -c /etc/kolla/globals.yml -n docker_namespace -v "$kolla_containers_namespace"
+    python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n docker_namespace -v "$kolla_containers_namespace"
 fi
 
-config_build_file=../config-build.yml
-if [ ! -f $config_build_file ]; then
-    cp $config_build_file.sample $config_build_file
-fi
-
-docker_registry=`python $get_config_value -c $config_build_file -n docker_registry`
+docker_registry=$(python "$GET_CONFIG_VALUE_SCRIPT" -c "$CONFIG_BUILD_FILE" -n docker_registry)
 if [ "$docker_registry" ]; then
-    python $set_config_value -c /etc/kolla/globals.yml -n docker_registry -v $docker_registry
+    python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n docker_registry -v $docker_registry
 fi
 
-python $set_config_value -c /etc/kolla/globals.yml -n enable_barbican -v yes
+docker_images_tag=$(python "$GET_CONFIG_VALUE_SCRIPT" -c "$CONFIG_BUILD_FILE" -n docker_images_tag)
+if [ "$docker_images_tag" ]; then
+    python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n openstack_release -v $docker_images_tag
+fi
+
+python $SET_CONFIG_VALUE_SCRIPT -c /etc/kolla/globals.yml -n enable_barbican -v yes
 
 kolla-ansible deploy -i ./kolla-ansible/ansible/inventory/coriolis
 kolla-ansible post-deploy
