b8742eb9c41865119c7ce771caf898d1bfbe91cc
Daniel Vincze <dvincze@cloudbasesolutions.com>
2020-07-15 11:12:12 +0000

    Move `_enable_oracle_repos` to core

--- coriolis/osmorphing/oracle.py
+++ coriolis/osmorphing/oracle.py
@@ -21,3 +21,23 @@ class BaseOracleMorphingTools(redhat.BaseRedHatMorphingTools):
     def _run_dracut(self):
         self._run_dracut_base('kernel')
         self._run_dracut_base('kernel-uek')
+
+    def _enable_oracle_repos(self):
+
+        major_version = int(self._version.split(".")[0])
+        if major_version < 8:
+            self._yum_install(['yum-utils'])
+            # TODO(apilotti): for ULN users, use the corresponding repos
+            # e.g.: ol7_x86_64_addons
+            self._exec_cmd_chroot(
+                "yum-config-manager --add-repo "
+                "http://public-yum.oracle.com/public-yum-ol%s.repo" %
+                major_version)
+
+            self._enable_repos = ["ol%s_software_collections" % major_version,
+                                  "ol%s_addons" % major_version]
+        else:
+            self._yum_install(['oraclelinux-release-el%s' % major_version])
+            self._exec_cmd_chroot(
+                "yum config-manager --enable ol%(release)s_appstream "
+                "ol%(release)s_UEKR6" % {"release": major_version})
